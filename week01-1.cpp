c0e1bc81d1d1f8f3b7ab78661333a8aaa196cf96
Sunnywang1001 <kent721595@gmail.com>
2025-02-19 11:58:36 +0800

    week01

---
+++ week01-1.cpp
@@ -0,0 +1,20 @@
+///week01-1.cpp 瘋狂程設 兩數相加
+///File-New empty 開新的檔案
+///Ctrl-S 存檔成week01-1.cpp 要記的加 .cpp
+///下周考試考這題,不用寫藍色的中文註解
+#include <stdio.h>///檔名不能寫錯
+///stdio 的意思是 標準的standaed input/output
+///有了stdio.h的外掛,才能用
+
+int main()
+{
+	int a, b;
+	scanf( "%d" , &a);
+	scanf( "%d" , &b);
+
+	printf("%d",a+b );
+
+
+}
+
+
